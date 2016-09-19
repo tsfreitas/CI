71e65cbaba532b56cc58d93a2462d40b390fd485
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-09-19 17:54:55 -0300

    Formatação de código

--- pipeline/vars/codeQuality.groovy
+++ pipeline/vars/codeQuality.groovy
@@ -1,7 +1,7 @@
 def call() {
-	// Lê o settings global do Jenkins
-	configFileProvider([configFile(fileId: "maven-settings", variable: "MAVEN_SETTINGS")]) {
-		// Executa comando maven
-		sh "${tool 'Maven'}/bin/mvn -s $env.MAVEN_SETTINGS sonar:sonar"
-	}
+    // Lê o settings global do Jenkins
+    configFileProvider([configFile(fileId: "maven-settings", variable: "MAVEN_SETTINGS")]) {
+        // Executa comando maven
+        sh "${tool 'Maven'}/bin/mvn -s $env.MAVEN_SETTINGS sonar:sonar"
+    }
 }
--- pipeline/vars/mavenPackage.groovy
+++ pipeline/vars/mavenPackage.groovy
@@ -1,12 +1,12 @@
 def call() {
-	// Lê o settings global do Jenkins
-	configFileProvider([configFile(fileId: "maven-settings", variable: "MAVEN_SETTINGS")]) {
-		try {
-			// Executa comando maven
-			sh "${tool 'Maven'}/bin/mvn -s $env.MAVEN_SETTINGS clean org.jacoco:jacoco-maven-plugin:prepare-agent package"
-		} finally {
-			// Gera relatório do Junit
-			junit "**/target/surefire-reports/*.xml"
-		}
-	}
+    // Lê o settings global do Jenkins
+    configFileProvider([configFile(fileId: "maven-settings", variable: "MAVEN_SETTINGS")]) {
+        try {
+            // Executa comando maven
+            sh "${tool 'Maven'}/bin/mvn -s $env.MAVEN_SETTINGS clean org.jacoco:jacoco-maven-plugin:prepare-agent package"
+        } finally {
+            // Gera relatório do Junit
+            junit "**/target/surefire-reports/*.xml"
+        }
+    }
 }
