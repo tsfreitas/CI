c9af1bc9b631a3c652f9bd9bd543b5dc1b2495d1
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-09-19 23:31:20 -0300

    [PIPELINE] - melhorando funçao de envio para o S3

--- pipeline/src/com/tsfreitas/AwsS3Actions.groovy
+++ pipeline/src/com/tsfreitas/AwsS3Actions.groovy
@@ -8,10 +8,14 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 
 class AwsS3Actions implements Serializable {
 
-    def s3Client
+    def bucketName
+    def accessKey
+    def secretKey
 
-    AwsS3Actions(accessKey, secretKey) {
-        s3Client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+    AwsS3Actions(accessKey, secretKey, bucketName) {
+        this.bucketName = bucketName
+        this.accessKey = accessKey
+        this.secretKey = secretKey
     }
 
     def archiveArtifact(artifact) {
@@ -21,13 +25,17 @@ class AwsS3Actions implements Serializable {
         println "Artefato irá subir para repositório em $fileName"
 
         // Faz upload do arquivo
-        def request = new PutObjectRequest('artifact-reposuitory', fileName, artifact.file)
-        s3Client.putObject(request)
+        def request = new PutObjectRequest(bucketName, fileName, artifact.file)
+        _client().putObject(request)
 
         println "Artefato armazenado com sucesso"
 
     }
 
+    @NonCPS
+    AmazonS3Client _client() {
+        return new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+    }
 
     String _createFileName(artifact) {
         // Nome do arquivo deve seguir o padrão /projectName/System/version
--- pipeline/vars/uploadArtifact.groovy
+++ pipeline/vars/uploadArtifact.groovy
@@ -1,8 +1,9 @@
 /**
  * Created by tsfreitas on 19/09/16.
  */
+import com.tsfreitas.AwsS3Actions
 
 def call(body) {
-    def s3Action = new com.tsfreitas.AwsS3Actions(env.accesKey,env.secretKey)
+    def s3Action = new AwsS3Actions(env.AWS_ACCESS_KEY,env.AWS_SECRET_KEY,env.AWS_BUCKET_NAME)
     s3Action.archiveArtifact(body)
 }
